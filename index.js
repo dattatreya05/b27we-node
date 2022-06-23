6f522efa7277830bee2140a490ad843e8755cafd
dattatreya05 <dattatreyamamidipaka@gmail.com>
2022-06-23 12:33:39 +0530

    movies route

--- index.js
+++ index.js
@@ -122,52 +122,7 @@ app.get("/", function (request, response) {
 //   response.send(movies);
 // });
 
-// Curosor -> pagination
-app.get("/movies", async function (request, response) {
-  //db.movies.find({})
-  const movies = await getAllMovies(); // converts cursor to array
-  response.send(movies);
-});
-
-// :id we are passing to get only that movie details
-app.get("/movies/:id", async function (request, response) {
-  console.log("request.params", request.params);
-  const { id } = request.params;
-  //db.movies.findOne({id: "104"})
-  const movie = await getMovieById(id);
-  console.log(movie);
-  // const movie = movies.find((mv) => mv.id === id);
-  movie
-    ? response.send(movie)
-    : response.status(404).send({ message: "No such movie found 😊" });
-});
-
-app.delete("/movies/:id", async function (request, response) {
-  console.log("request.params", request.params);
-  const { id } = request.params;
-  //db.movies.deleteOne({id: "102"})
-  const result = await deleteMovieById(id);
-  console.log(result);
-  response.send(result);
-});
-
-app.put("/movies/:id", async function (request, response) {
-  console.log("request.params", request.params);
-  const { id } = request.params;
-  const updateData = request.body;
-  //db.movies.updateOne({id: "104"}, {$set: updateData})
-  const result = await updateMovieById(id, updateData);
-  console.log(result);
-  response.send(result);
-});
-
-app.post("/movies", async function (request, response) {
-  const newMovies = request.body;
-  console.log(newMovies);
-  // db.movies.insertMany(data)
-  const result = await createMovies(newMovies);
-  response.send(result);
-});
+app.use('/movies', moviesRouter);
 
 app.listen(PORT, () => console.log(`Server is started in ${PORT}`));
 
